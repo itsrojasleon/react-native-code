300c18910355310f7c21e25d19052085ce744324
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-09 18:44:54 -0500

    Touchable opaccity in action

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
@@ -9,6 +9,9 @@ const HomeScreen = () => {
         title="Go to Components Demo"
         onPress={() => console.log('Button pressed')}
       />
+      <TouchableOpacity onPress={() => console.log('Pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
