0f1ac0c0d7f7c907a198b775b06c0c967a86d924
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-09 16:24:08 -0500

    Building List

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function ListScreen() {
+  return (
+    <View>
+      <Text>List Screen</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
+export default ListScreen;
