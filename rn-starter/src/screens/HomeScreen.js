ce411b2d4b5a7a0f43c88cb254af055ba611e806
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-09 18:40:22 -0500

    Button in Action

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => console.log('Button pressed')}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
