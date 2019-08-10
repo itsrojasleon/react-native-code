dde8d1edf85f0c7cac414856ca6e32ed0d44f136
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-09 20:40:28 -0500

    Image Screen

---
+++ rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>Image Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+export default ImageScreen;
