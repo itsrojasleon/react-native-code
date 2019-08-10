d150a1434edbf4aea03866fdb530629e1c2f94dc
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-10 17:07:19 -0500

    Adding images

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
   return (
     <View>
-      <Text>Image Screen</Text>
+      <ImageDetail
+        title="Forest"
+        imageSource={require('../../assets/forest.jpg')}
+        score={7}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require('../../assets/beach.jpg')}
+        score={7}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require('../../assets/mountain.jpg')}
+        score={7}
+      />
     </View>
   );
 };
