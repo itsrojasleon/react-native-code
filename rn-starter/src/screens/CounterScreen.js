6a0ea891ad93ab9ae46eb048a6d75e7a1d0bb85a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-10 17:12:31 -0500

    Screen Boilerplate

---
+++ rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+function CounterScreen() {
+  return (
+    <View>
+      <Text>Current Count</Text>
+    </View>
+  );
+}
+export default CounterScreen;
