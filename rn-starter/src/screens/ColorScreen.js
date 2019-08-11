557d2a357270a41d2e1ed5e8459fb07445d76c42
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-11 14:17:58 -0500

    Color

---
+++ rn-starter/src/screens/ColorScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function ColorScreen() {
+  return (
+    <View>
+      <Button title="Add a Color" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
