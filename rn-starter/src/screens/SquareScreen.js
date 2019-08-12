1316c00d4c45ff6828ccff9a5d4d99a36056e9d5
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-12 14:16:08 -0500

    Validating state changes

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -8,12 +8,26 @@ const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
   const [green, setGreen] = useState(0);
+
+  const setColor = (color, change) => {
+    // color === 'red', 'green', 'blue'
+    // change === +15, -15
+
+    if (color === 'red') {
+      if (red + change > 255 || red + change < 0) {
+        return;
+      } else {
+        setRed(red + change);
+      }
+    }
+  };
+
   return (
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
       />
       <ColorCounter
         color="Blue"
