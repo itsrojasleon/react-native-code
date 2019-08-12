af6dd35f928e46967f5ffa7eee5f4e2142b64317
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-11 20:06:46 -0500

    Reusable Color Adjusters

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
   return (
     <View>
-      <Text>Square Screen</Text>
+      <ColorCounter color="Red" />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
     </View>
   );
 };
