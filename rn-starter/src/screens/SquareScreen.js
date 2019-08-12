7722a7df8daa150344776d192298d7ab29f807fc
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-11 19:59:52 -0500

    Square

---
+++ rn-starter/src/screens/SquareScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SquareScreen = () => {
+  return (
+    <View>
+      <Text>Square Screen</Text>
+    </View>
+  );
+};
+export default SquareScreen;
