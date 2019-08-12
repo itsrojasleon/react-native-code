dbf962f1517d92edc88d30e685d8c32053464439
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-12 14:03:52 -0500

    Tying state values together

--- rn-starter/src/components/ColorCounter.js
+++ rn-starter/src/components/ColorCounter.js
@@ -5,8 +5,8 @@ const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button onPress={() => onIncrease} title={`Increase ${color}`} />
-      <Button onPress={() => onDecrease} title={`Decrease ${color}`} />
+      <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
+      <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
   );
 };
--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
@@ -10,18 +12,25 @@ const SquareScreen = () => {
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
       />
       <ColorCounter
         color="Blue"
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
       />
       <ColorCounter
         color="Green"
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${blue}, ${green})`
+        }}
       />
     </View>
   );
