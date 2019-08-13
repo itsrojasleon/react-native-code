95f0a34988f3c06829eef20489c80ba721d98a6f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-13 17:00:47 -0500

    Restoring validation

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -10,11 +10,18 @@ const reducer = (state, action) => {
 
   switch (action.colorToChange) {
     case 'red':
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
     case 'green':
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 ||
+        state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
     case 'blue':
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
