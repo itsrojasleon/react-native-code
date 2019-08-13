b449a6d1b1dfcdbd476de3623fdd35a4618b1ed1
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-13 16:51:13 -0500

    Applying state with a reducer

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -10,35 +10,48 @@ const reducer = (state, action) => {
 
   switch (action.colorToChange) {
     case 'red':
-      return {...state, red: state.red + action.amount}
+      return { ...state, red: state.red + action.amount };
     case 'green':
-      return {...state, green: state.green + action.amount}
+      return { ...state, green: state.green + action.amount };
     case 'blue':
-      return {...state, blue: state.blue + action.amount}
+      return { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
-}
+};
 
 const SquareScreen = () => {
-  const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state;
 
   return (
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => }
-        onDecrease={() =>}
+        onIncrease={() =>
+          dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })
+        }
       />
       <ColorCounter
         color="Blue"
-        onIncrease={() => }
-        onDecrease={() =>}
+        onIncrease={() =>
+          dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })
+        }
       />
       <ColorCounter
         color="Green"
-        onIncrease={() => }
-        onDecrease={() =>}
+        onIncrease={() =>
+          dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })
+        }
       />
       <View
         style={{
