e91555c0fe24c14e7673c064ac68892a707a9df2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-16 16:56:33 -0500

    Starting the SearchBar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -3,9 +3,17 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
       <Text>Search Screen</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#f0eeee',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  },
+});
 export default SearchBar;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar />
     </View>
   );
 };
