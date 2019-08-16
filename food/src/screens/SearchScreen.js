8e900af80c6e22e35e4402d80afa9463430d9484
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-16 16:39:55 -0500

    Assembling a Navigator

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+export default SearchScreen;
