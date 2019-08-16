7b9808174dfee3763a560e33a983011b055ad2ef
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-16 17:34:40 -0500

    Displaying Icons

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Feather name='search' size={30} />
       <Text>Search Screen</Text>
     </View>
   );
