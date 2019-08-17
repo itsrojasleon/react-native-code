aeb2188d8ae2d57ee155ec44aaeef46d85ee4e78
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-16 19:42:17 -0500

    Error Handling

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -14,7 +14,6 @@ const SearchBar = ({ inputProps, onTermSubmit }) => {
         style={styles.inputStyle}
         placeholder='Search'
       />
-      <Text>{inputProps.value}</Text>
     </View>
   );
 };
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,21 +8,27 @@ const SearchScreen = () => {
   // input props will be {value: '', onChangeText: function}
   const { inputProps } = useTextInput('');
   const [results, setResults] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 5,
-        term: inputProps.value,
-        location: 'san jose',
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 5,
+          term: inputProps.value,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
     <View>
       <SearchBar inputProps={inputProps} onTermSubmit={searchApi} />
+      {errorMessage && <Text>{errorMessage}</Text>}
       <Text>We have found {results.length} results</Text>
     </View>
   );
