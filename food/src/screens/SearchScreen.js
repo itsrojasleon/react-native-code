ee68d4a63bce2eb641f214420c2b7fcba77904f1
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-18 15:51:52 -0500

    Showing Search Results

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -4,21 +4,21 @@ import SearchBar from '../components/SearchBar';
 import { useTextInput } from '../hooks/useTextInput';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   // input props will be {value: '', onChangeText: function}
   const { inputProps } = useTextInput('');
   const [searchApi, results, errorMessage] = useResults();
 
-  // Call searchApi when component
-  // is first rendered.   BAD CODE!
-  // searchApi('pasta');
-
   return (
     <View>
       <SearchBar inputProps={inputProps} onTermSubmit={() => searchApi(term)} />
       {errorMessage && <Text>{errorMessage}</Text>}
       <Text>We have found {results.length} results</Text>
+      <ResultsList title='Cost Effective' />
+      <ResultsList title='Bit Pricier' />
+      <ResultsList title='Big Spender' />
     </View>
   );
 };
