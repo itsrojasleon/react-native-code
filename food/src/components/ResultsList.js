bf38aae25a2d3a3dd432981aead216e50951bc83
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-20 00:20:59 -0500

    One more Last Fix

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -10,6 +10,8 @@ import ResultsDetail from './ResultsDetail';
 import { withNavigation } from 'react-navigation';
 
 const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) return null;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -15,13 +15,27 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
+  if (!result) return null;
+
   return (
     <View>
-      <Text>Results Show</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => (
+          <Image style={styles.image} source={{ uri: item }} />
+        )}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultsShowScreen;
