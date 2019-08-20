ddabd2b477ded995d8bbaaff18e1add24ee14f10
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-20 17:05:04 -0500

    Initial Setup

--- blog/App.js
+++ blog/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
   },
-});
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+export default createAppContainer(navigator);
---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => (
+  <View>
+    <Text>ok</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
