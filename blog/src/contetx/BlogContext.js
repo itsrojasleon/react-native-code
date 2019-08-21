3f7b7740f73a31e110b1c395680993d0937a5893
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-21 18:36:47 -0500

    Rendering a List of Posts

--- blog/src/contetx/BlogContext.js
+++ blog/src/contetx/BlogContext.js
@@ -3,6 +3,9 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+  const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
 export default BlogContext;
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../contetx/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
   return (
     <View>
-      <Text>The value is: {value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
