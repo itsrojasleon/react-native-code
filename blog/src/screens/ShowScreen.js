2df9c40fed700b5834e42f49d2ac152866aec851
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-22 18:28:05 -0500

    The Edit Icon Link

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../contetx/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -10,8 +11,19 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = () => {
+  return {
+    headerRight: (
+      <TouchableOpacity>
+        <EvilIcons name="pencil" size={30} />
+      </TouchableOpacity>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
