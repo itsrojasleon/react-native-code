bcd1771124245158b869880310bbfc46e7206688
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-22 15:51:50 -0500

    Header Navigation

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -39,6 +39,16 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    )
+  };
+};
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
