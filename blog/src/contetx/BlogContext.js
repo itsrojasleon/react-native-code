2cb5be493d355f002165ea69201bf66e24fc0b60
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-22 20:04:23 -0500

    Navigating Backwards

--- blog/src/contetx/BlogContext.js
+++ blog/src/contetx/BlogContext.js
@@ -25,7 +25,7 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
   return (title, content, callback) => {
     dispatch({ type: 'add_blogpost', payload: { title, content } });
-    callback();
+    if (callback) callback();
   };
 };
 const deleteBlogPost = dispatch => {
@@ -34,8 +34,9 @@ const deleteBlogPost = dispatch => {
   };
 };
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
+  return (id, title, content, callback) => {
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+    if (callback) callback();
   };
 };
 
