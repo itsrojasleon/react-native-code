4527ef8a325d29fafa40dd3883446bd45301b7a5
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-27 17:25:38 -0500

    Wiring Up JSON Web Tokens

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -24,8 +26,8 @@ mongoose.connection.on('error', error => {
   console.log('Error connecting to Mongo', error);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email is ${req.user.email}`);
 });
 
 app.listen(3000, () => {
