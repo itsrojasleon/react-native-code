469d5c641107388ff8990cbebfc75c41acabaca3
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-27 16:48:33 -0500

    Creating a JWT

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const User = mongoose.model('User');
 
@@ -7,12 +8,12 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = new User({ email, password });
     await user.save();
 
-    res.send('You made a post request');
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
   } catch (err) {
     res.status(422).send(err.message);
   }
