3204048e82f847c762502865d0484e86dd669d50
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-26 19:33:45 -0500

    Connecting to MongoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://rojasleon:D9SEMGBUZfABF9ZL@cluster0-13ox1.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', error => {
+  console.log('Error connecting to Mongo', error);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
