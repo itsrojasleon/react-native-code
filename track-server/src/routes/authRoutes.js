fda79b9dee46e9decc03eae1a7ea15438bb7ef78
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-27 16:20:23 -0500

    Error Handling

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -5,8 +5,16 @@ const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.send('You made a post request');
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    res.send('You made a post request');
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
 });
 module.exports = router;
