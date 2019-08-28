55a9c2332ef4060422d8303cc4abf3f12c77d1ed
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-28 13:10:54 -0500

    Listing Tracks

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRouter);
+app.use(trackRoutes);
 
 const mongoUri =
   'mongodb+srv://rojasleon:D9SEMGBUZfABF9ZL@cluster0-13ox1.mongodb.net/test?retryWrites=true&w=majority';
---
+++ track-server/src/routes/trackRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../middlewares/requireAuth');
+
+const Track = mongoose.model('Track');
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get('/tracks', async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
+});
+module.exports = router;
