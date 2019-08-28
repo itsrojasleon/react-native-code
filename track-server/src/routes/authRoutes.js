2bd7e834d9a75cae3b403b2d400e35d60fc5f0a3
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-27 23:07:27 -0500

    The signin Route

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -18,4 +18,27 @@ router.post('/signup', async (req, res) => {
     res.status(422).send(err.message);
   }
 });
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).send({ error: 'Email not found' });
+  }
+
+  try {
+    await user.comparePassword(password);
+
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+});
 module.exports = router;
