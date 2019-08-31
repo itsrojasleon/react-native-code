99463c521311511227b82200c8c0981c4b0d7f6f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-08-30 19:53:52 -0500

    Navigation to Signin

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/authContext';
@@ -36,6 +36,11 @@ const SignupScreen = ({ navigation }) => {
       <Spacer>
         <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+        <Spacer>
+          <Text style={styles.link}>Already have an account?</Text>
+        </Spacer>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -58,6 +63,9 @@ const styles = StyleSheet.create({
     color: 'red',
     marginLeft: 15,
     marginTop: 15
+  },
+  link: {
+    color: 'blue'
   }
 });
 
