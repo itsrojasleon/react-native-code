02d842ff3045d91d0c511ee7b73eaf8c187f30f8
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-01 17:02:20 -0500

    Wiring Up Signin

--- tracks/src/context/authContext.js
+++ tracks/src/context/authContext.js
@@ -7,7 +7,7 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
-    case 'signup':
+    case 'signin':
       return { token: action.payload, errorMessage: '' };
     default:
       return state;
@@ -18,7 +18,7 @@ const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: response.data.token });
+    dispatch({ type: 'signin', payload: response.data.token });
 
     navigate('TrackList');
   } catch (err) {
@@ -29,8 +29,18 @@ const signup = dispatch => async ({ email, password }) => {
   }
 };
 
-const signin = dispatch => {
-  return ({ email, password }) => {};
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signin', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signin', payload: response.data.token });
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with Sign in'
+    });
+  }
 };
 
 const signout = dispatch => {
--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context } from '../context/authContext';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(Context);
   return (
     <View style={styles.container}>
       <AuthForm
         headerText="Sign in to your account"
-        errorMessage=""
-        onSubmit={() => {}}
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
         submitButtonText="Signin"
       />
       <NavLink
