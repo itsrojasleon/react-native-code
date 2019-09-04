b807bbfea3c86822837b72e7eb60d21869a063f3
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 17:12:14 -0500

    Extracting logic to a Hook

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -12,6 +12,7 @@ const locationReducer = (state, action) => {
 const startRecording = dispatch => () => {};
 const stopRecording = dispatch => () => {};
 const addLocation = dispatch => location => {
+  console.log('Moving...');
   dispatch({ type: 'add_current_location', payload: location });
 };
 
