ac4183947ee701b83f92b559cf09dfd6376a66ef
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-04 19:55:25 -0500

    Automatic Authentication

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: ' http://9280476a.ngrok.io'
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
