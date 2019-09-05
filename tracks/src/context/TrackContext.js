a6d08ce118af24ec48dc11cc2c52f26d6acc6afe
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-05 14:14:07 -0500

    Fetching created Tracks

--- tracks/src/context/TrackContext.js
+++ tracks/src/context/TrackContext.js
@@ -3,12 +3,17 @@ import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case 'fetch_tracks':
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  const response = await trackerApi.get('/tracks');
+  dispatch({ type: 'fetch_trakcs', payload: response.data });
+};
 const createTrack = dispatch => async (name, locations) => {
   try {
     await trackerApi.post('/tracks', { name, locations });
