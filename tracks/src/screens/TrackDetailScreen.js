2f1618836472f9e889b8203bf1337a759435725c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-09-05 15:10:31 -0500

    Navigating to a Saved Track

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam('_id');
+
+  const track = state.find(t => t._id === _id);
+
   return (
-    <View>
-      <Text>TrackDetailScreen</Text>
-    </View>
+    <>
+      <Text>{track.name}</Text>
+    </>
   );
 };
 
